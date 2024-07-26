b5fa9cb2e3b80a329fd1de41727b2e17f1c00f51
wafa kazi <wafakaziofficial99@gmail.com>
2024-07-26 22:35:14 +0530

    Done

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const calculatorInput = document.querySelector('input');
-const buttons = document.querySelectorAll('.buttons');
+const calculatorInput = document.querySelector(".input");
+const buttons = document.querySelectorAll(".button");
 
 Array.from(buttons).forEach((button) => {
-  button.addEventListener('click', (e) => {
+  button.addEventListener("click", (e) => {
     const buttonValue = e.target.innerHTML;
 
-    if (buttonValue === '=') {
+    if (buttonValue === "=") {
       try {
         const result = evaluateExpression(calculatorInput.value);
         calculatorInput.value = result;
       } catch (error) {
-        calculatorInput.value = 'Error';
+        calculatorInput.value = "Error";
       }
-    } else if (buttonValue === 'C') {
-      calculatorInput.value = '';
+    } else if (buttonValue === "C") {
+      calculatorInput.value = "";
+    } else if (buttonValue === "AC") {
+      calculatorInput.value = "";
     } else {
       calculatorInput.value += buttonValue;
     }
@@ -21,6 +23,5 @@ Array.from(buttons).forEach((button) => {
 });
 
 function evaluateExpression(expression) {
- 
   return eval(expression);
 }
